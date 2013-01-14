8a9c148ff1d9b975e475b2a2675ac68f8aececbe
Nick Brown <nwbrown@gmail.com>
2013-01-13 23:24:34 -0500

    add normalize function

--- src/ml_clj/utils.clj
+++ src/ml_clj/utils.clj
@@ -8,3 +8,21 @@
           (if (or (empty? as) (empty? bs)) sum
             (recur ($= ((first as) - (first bs)) ** 2 + sum)
                    (rest as) (rest bs))))))
+
+(let [normalize-field
+      (fn [field]
+          (let [col ($ field)
+                max-val (apply max col)
+                min-val (apply min col)
+                norm (fn [v]
+                       (if (= max-val min-val) 0
+                       ($= (v - min-val) / (max-val - min-val))))]
+            ($map norm field)))]
+
+  (defn normalize
+    "normalize a set of fields in a data set, each to be labeled field'"
+    [data & fields]
+    (with-data data
+      (col-names
+        (apply conj-cols data (map normalize-field fields))
+        (concat (col-names data) (map #(keyword (str (name %) "'")) fields))))))
--- test/ml_clj/utils_test.clj
+++ test/ml_clj/utils_test.clj
@@ -1,5 +1,6 @@
 (ns ml-clj.utils-test
   (:use clojure.test
+        incanter.core
         ml-clj.utils))
 
 (deftest test-euclidean
@@ -10,3 +11,30 @@
                   (euclidean [0 0] [3 4])
                   (euclidean [1 1] [4 5])))))
 
+(defn v= [& vectors]
+  (loop [vs vectors]
+    (if (empty? (first vs)) true
+      (if (apply == (map first vs))
+        (recur (map rest vs))
+        false))))
+
+(deftest test-normalize
+  (testing "That it normalizes a set of data"
+           (let [d (dataset [:big :little]
+                            [[42 0.042]
+                             [64 0.064]
+                             [0 0.000]
+                             [100 0.100]])
+                 d' (normalize d :big :little)]
+             (is (v= [0.42 0.64 0.0 1.0]
+                     ($ :big' d')
+                     ($ :little' d')))))
+  (testing "That it handles data that does not vary"
+           (let [d (dataset [:same :different]
+                            [[5 1]
+                             [5 1.5]
+                             [5 2]
+                             [5 1.25]])
+                 d' (normalize d :same :different)]
+             (is (v= [0 0.5 1 0.25] ($ :different' d')))
+             (is (v= [0 0 0 0] ($ :same' d'))))))
