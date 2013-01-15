f5c688663a175d67bf77e9a189fe1a5d6bd1904e
Nick Brown <nwbrown@gmail.com>
2013-01-14 23:05:40 -0500

    Use more traditional private defn

--- src/ml_clj/nkk.clj
+++ src/ml_clj/nkk.clj
@@ -2,16 +2,24 @@
   (:use (incanter core)
         (ml-clj utils)))
 
-(let [dist-func (fn [point] (fn [& other] (euclidean point other))),
-      compute-dists (fn [from-point data features]
-                     (let [dist-col ($map (dist-func from-point) features data)]
-                       (col-names (conj-cols data dist-col)
-                                  (conj (col-names data) :dist))))]
-  (defn classify
-    "use the knn classification algorithm to classify the new datum"
-    [to-classify data features label k]
-    (with-data (compute-dists to-classify data features)
-               ($ 0 :type 
-                  ($order :votes :desc 
-                     ($rollup :count :votes :type 
-                        ($ (range k) [label :dist] ($order :dist :asc))))))))
+(defn- dist-func
+  "Create a distance function for the distance to a point"
+  [point]
+  (fn [& other] (euclidean point other)))
+
+(defn- compute-dists
+  "compute the distances from a data point to a data set"
+  [from-point data features]
+  (let [dist-col ($map (dist-func from-point) features data)]
+    (col-names (conj-cols data dist-col)
+               (conj (col-names data) :dist))))
+
+(defn classify
+  "use the knn classification algorithm to classify the new datum"
+  [to-classify data features label k]
+  ($ 0 :type
+     ($order :votes :desc
+        ($rollup :count :votes :type
+           ($ (range k) [label :dist]
+              ($order :dist :asc
+                 (compute-dists to-classify data features)))))))
